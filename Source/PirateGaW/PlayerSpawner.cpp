a6db89c2d10b4d9b17527e906a1b23876193b645
Cnmhqwerty <68733738+cnmhqwerty@users.noreply.github.com>
2023-04-25 16:00:52 +0100

    Added more spawns

---
+++ Source/PirateGaW/PlayerSpawner.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerSpawner.h"
+
+PlayerSpawner::PlayerSpawner()
+{
+}
+
+PlayerSpawner::~PlayerSpawner()
+{
+}
